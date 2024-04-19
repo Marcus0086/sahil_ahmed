f1e27a1252fe3e64a3df55692e7d39a056e328e3
Marcus0086 <guptamarcus42@gmail.com>
2024-04-19 18:10:04 +0530

    Update name in nav component

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -16,7 +16,7 @@ const Nav = () => {
   return (
     <header className="px-4 lg:px-6 h-14 flex items-center">
       <Link className="flex items-center justify-center" href="/">
-        <span className="">Sahil Ahmed</span>
+        <span className="">Sumit Ahmed</span>
       </Link>
       <nav className="ml-auto flex gap-4 sm:gap-6">
         <Link
